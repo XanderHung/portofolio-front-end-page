5a6d7ac7a9f272355746bdbb7f69a05ba71ec244
Hung <alexhung082008@gmail.com>
2023-08-31 14:13:49 +0700

    [refine] add some transition in blog page

--- src/modules/blog/blog-content.tsx
+++ src/modules/blog/blog-content.tsx
@@ -1,5 +1,6 @@
-import { Center, Heading } from "@chakra-ui/react";
+import { Box, Center, Heading } from "@chakra-ui/react";
 import Head from "next/head";
+import { motion } from "framer-motion";
 
 export default function BlogContent() {
   return (
@@ -8,8 +9,16 @@ export default function BlogContent() {
         <title>Blog</title>
       </Head>
       <Heading fontSize="5xl">Blog</Heading>
-      <Center as={"b"} fontSize={"4xl"}>
-        Coming Soon
+      <Center as={"b"}>
+        <Box
+          as={motion.div}
+          fontSize={"4xl"}
+          drag="y"
+          dragConstraints={{ top: -100, bottom: 100 }}
+          transition="5s linear"
+        >
+          Coming Soon
+        </Box>
       </Center>
     </>
   );
